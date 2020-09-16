1b916c1ae8a9eb2a8178d5ba55ebf0df7aa8ad5f
Tom lima <tom@sabiorealm.com>
2020-09-16 16:44:41 -0500

    Translate modules and leccions words

--- application/helpers/production_helper.php
+++ application/helpers/production_helper.php
@@ -18,7 +18,7 @@ function activeSession()
 {
   $CI = &get_instance();
   if (production() == false) {
-    return false;
+    return true;
   } else {
     $sess_id = $CI->session->userdata('id');
     if ($sess_id) {
