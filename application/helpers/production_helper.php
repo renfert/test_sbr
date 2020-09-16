2fedf4b9f708042c70fd501ed9e8720bf1f42d93
Tom lima <tom@sabiorealm.com>
2020-09-16 11:07:45 -0500

    Fix login button on product page

--- application/helpers/production_helper.php
+++ application/helpers/production_helper.php
@@ -18,7 +18,7 @@ function activeSession()
 {
   $CI = &get_instance();
   if (production() == false) {
-    return true;
+    return false;
   } else {
     $sess_id = $CI->session->userdata('id');
     if ($sess_id) {
