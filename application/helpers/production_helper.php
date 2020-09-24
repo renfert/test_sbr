4bf3981c385f5c0ed9b4374482e5036878ee34af
Tom lima <tom@sabiorealm.com>
2020-09-24 12:11:53 -0500

    Change production helper

---
+++ application/helpers/production_helper.php
@@ -0,0 +1,34 @@
+<?php
+function production()
+{
+  if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function getUserId()
+{
+  $CI = &get_instance();
+  if (production() == false) {
+    return 4;
+  } else {
+    return $CI->session->userdata("id");
+  }
+}
+
+function activeSession()
+{
+  $CI = &get_instance();
+  if (production() == false) {
+    return true;
+  } else {
+    $sess_id = $CI->session->userdata('id');
+    if ($sess_id) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
--- application/models/Company_Model.php
+++ application/models/Company_Model.php
@@ -1,25 +1,27 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Company_Model extends CI_Model {
+class Company_Model extends CI_Model
+{
 
-	public function __construct(){
-		parent::__construct();
-	}
+  public function __construct()
+  {
+    parent::__construct();
+  }
 
-	/* 
+  /*
     =============================================
     Returns: Array with only one object
-    ============================================== 
-    */  
-	public function getCompanyInformation(){
-		$this->db->select("*");
-		$result=$this->db->get("mycompany");
-		if($result->num_rows()>0){
-			return $result->row();
-		}
-		else{
-			return false;
-		}
-	}
+    ==============================================
+    */
+  public function getCompanyInformation()
+  {
+    $this->db->select("*");
+    $result = $this->db->get("mycompany");
+    if ($result->num_rows() > 0) {
+      return $result->row();
+    } else {
+      return false;
+    }
+  }
 }
